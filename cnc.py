210b2ce1ae39028a6d5257a9e5c6e789ba2f6f34
tjhowse <tjhowse@gmail.com>
2020-05-12 21:18:25 +1000

    On second thoughts, let's not. That might cause weird stepping along the edges.

--- cnc.py
+++ cnc.py
@@ -104,7 +104,7 @@ def filter_coordinate(point, vertices):
     elif dist > 1:
         return b
     else:
-        return (math.trunc(a[0]+v_a_b[0]*dist), math.trunc(a[1]+v_a_b[1]*dist))
+        return (a[0]+v_a_b[0]*dist, a[1]+v_a_b[1]*dist)
 
 class cnc():
     move_mode = 0
