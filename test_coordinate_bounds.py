2c441dd095f0f1a485a62b3f8921d049ed493837
tjhowse <tjhowse@gmail.com>
2020-05-14 20:40:16 +1000

    To generate testing gcode that follows the border of the enclosure.

--- test_coordinate_bounds.py
+++ test_coordinate_bounds.py
@@ -2,6 +2,12 @@
 
 from constants import *
 from cnc import *
+gcode = ""
+
+for vert in ENCLOSURE_VERTICES:
+    gcode += "G1 X{} Y{},".format(vert[0], vert[1])
+
+print(gcode)
 print(ENCLOSURE_VERTICES)
 print(filter_coordinate((100,100), ENCLOSURE_VERTICES))
 print(filter_coordinate((150,180), ENCLOSURE_VERTICES))
